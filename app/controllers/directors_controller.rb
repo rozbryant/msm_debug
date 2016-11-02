c7656d4374f36b5aee22949b43d6ea03f12a4db8
Roz Bryant Dupiton <rozbryant@gmail.com>
2016-11-01 19:43:09 -0500

    Directors(Read) functional

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,6 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
@@ -12,6 +12,7 @@ class DirectorsController < ApplicationController
 
   def create_row
     @director = Director.new
+
     @director.dob = params[:dob]
     @director.name = params[:name]
     @director.bio = params[:bio]
@@ -29,10 +30,10 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
